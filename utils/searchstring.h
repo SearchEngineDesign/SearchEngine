cc00cc40720627b18ded79208f093f847515f8c1
darrinz <darrinz@umich.edu>
2025-04-17 21:38:00 -0400

    c_str() for string

--- utils/searchstring.h
+++ utils/searchstring.h
@@ -118,6 +118,10 @@ class string
          {
             return m_data;
          }
+      char *c_str()
+         {
+            return m_data;
+         }
 
       // Length member function
       // REQUIRES: Nothing
