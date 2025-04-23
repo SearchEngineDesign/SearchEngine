8c868eebf0b5626df5a4d795c88bcf12893b56e0
XciciciX <xixiaoxx@umich.edu>
2025-04-22 23:50:51 -0400

    add string for frontend

--- utils/searchstring.h
+++ utils/searchstring.h
@@ -560,8 +560,22 @@ class string
          return m_data;
       }
 
-      friend std::istream &operator>>( std::istream &is, string &s );
+      size_t npos() const
+      {
+         return -1;
+      }
 
+
+      size_t find_last_of(char ch) const {
+        for (size_t i = m_size; i > 0; --i) {
+            if (m_data[i - 1] == ch) {
+                return i - 1;
+            }
+        }
+        return static_cast<size_t>(-1); // cannot find, return -1
+      }
+
+      friend std::istream &operator>>( std::istream &is, string &s );
    private:
       size_t m_size;
       size_t m_capacity;
